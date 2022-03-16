cc08735a2f186bf328e9148eaf1ccc0af2bf978f
Sigma <sigmw@protonmail.com>
2022-03-16 19:23:34 -0300

    suckless :)

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use md5;
+use std::env;
+use std::fs;
+fn main() {
+    let mut args = env::args().into_iter();
+    args.next();
+    for i in args.next() {
+        match fs::read(&i) {
+            Ok(x) => {
+                println!("{:?} {}", md5::compute(x), i)
+            }
+            Err(x) => println!("md5checker: {}: {}", i, x),
+        }
+    }
+}
